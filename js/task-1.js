448e0e4b221d8212ee4a634a4f1426f628bba913
PavlykIryna <155089011+PavlykIryna@users.noreply.github.com>
2024-06-24 20:48:46 +0200

    finish02

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const categoriesList = document.querySelectorAll(`ul#categories .item`);
 console.log(`Number of categories: ${categoriesList.length}`);
 for (let category of categoriesList) {
-  const categoryItemsCount= category.querySelector(`ul`).children.length;
+  const categoryItemsCount = category.querySelector(`ul`).children.length;
   const categoryName = category.querySelector(`h2`).textContent;
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryItemsCount}`);
